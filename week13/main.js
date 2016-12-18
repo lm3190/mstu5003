9a15b88ea7e73c91d691599d4db1d1beb5335d7c
Lisa Sepahi <Lisa@Lisa-Sepahis-MacBook-Pro.local>
2016-12-17 21:53:31 -0500

    update

--- week13/main.js
+++ week13/main.js
@@ -4,3 +4,21 @@ var currentUser = {
 	last: "Kuwata",
 	id: "a1"
 };
+
+var userMsg = [];
+
+document.querySelector('#submit').addEventListner('click', function(){
+	userMsg.push(document.querySelector('#textinput').value);
+	alert(userMsg);
+	document.querySelector('#textinput').value = "";
+});
+
+
+// var userMsg = document.querySelector('#textinput').value;
+//
+// document.getElementById('submit').addEventListner('click', function(){
+// alert(userMsg);
+// document.querySelector('#textinput').value = "";
+// });
+//
+// };
